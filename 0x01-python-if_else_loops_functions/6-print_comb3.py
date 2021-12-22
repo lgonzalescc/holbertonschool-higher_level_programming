885de4023c221216a61b70f43a0a68b5eba450e9
Luis Gonzales <luisgonzacc@gmail.com>
2021-12-22 04:54:24 +0000

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for a in range(9):
-    for b in range(a, 10):
-        if a < b:
-            print("{:d}{:d}".format(a, b),
-                  end="\n" if a is 8 and b is 9 else ", ")
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
