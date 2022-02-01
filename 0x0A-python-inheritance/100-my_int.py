97ce716cf9862c0c17635a561adb6abfd9f32522
Luis <luisgonzacc@gmail.com>
2022-02-01 04:33:11 +0000

    Task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Module for MyInt class."""
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """
-    Reprensting a subclass of int class.
-    """
+    """Invert int operators == and !=."""
 
-    def __equal__(self, x):
-        """override and inverte equal."""
-        return int(self) != int(x)
+    def __eq__(self, value):
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
-    def __notequal__(self, x):
-        """override and inverte not equal."""
-        return int(self) == int(x)
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
