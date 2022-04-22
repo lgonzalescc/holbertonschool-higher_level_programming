3b90672c530fad99c396aff5e182de2c7715fabf
Luis Gonzales <luisgonzacc@gmail.com>
2022-04-22 21:18:03 +0000

    Task 6}

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm."""
+"""contains the function find_peak"""
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
-    if list_of_integers == []:
+    """finds a peak in a list of unsorted integers"""
+    if not list_of_integers:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    length = len(list_of_integers)
+    half = int(length / 2)
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+    # Validate Length only 3.
+    if half + 1 >= length and half - 1 < 0:
+        return list_of_integers[half]
+    elif half - 1 < 0:
+        if list_of_integers[half] > list_of_integers[half + 1]:
+            return list_of_integers[half]
+        else:
+            return list_of_integers[half + 1]
+    elif half + 1 >= length:
+        if list_of_integers[half] > list_of_integers[half - 1]:
+            return list_of_integers[half]
+        else:
+            return list_of_integers[half - 1]
+
+    if list_of_integers[half - 1] < list_of_integers[half]\
+       > list_of_integers[half + 1]:
+        return list_of_integers[half]
+
+    if list_of_integers[half + 1] > list_of_integers[half - 1]:
+        return find_peak(list_of_integers[half:])
+
+    return find_peak(list_of_integers[:half])
