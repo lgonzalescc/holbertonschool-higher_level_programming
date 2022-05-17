4b3fa76331bc3b8db70ab5a9819939532c790bbe
Luis Gonzales <luisgonzacc@gmail.com>
2022-05-17 23:42:15 +0000

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
+// This script reads and prints the content of a file.
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-    console.log(error || content);
+fs.readFile(process.argv[2], 'utf8', function (err, f) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(f);
 });
