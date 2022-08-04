b36ad0a30b66d0cdbc7d7c501cf3bbbab2129036
Luis Gonzales <luisgonzacc@gmail.com>
2022-08-04 10:13:42 -0500

    Task 13

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+const Square1 = require('./5-square.js');
 
-  charPrint (c = 'X') {
-    this.print(c);
+module.exports = class Square extends Square1 {
+  charPrint (C) {
+    if (C === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(C.repeat(this.height));
+      }
+    }
   }
-}
-module.exports = Square;
+};
