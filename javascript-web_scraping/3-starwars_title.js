5a5d521242a9915c1f1b94746af9f33c97594e36
Luis Gonzales <luisgonzacc@gmail.com>
2023-04-21 15:55:31 -0500

    fix file

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -6,4 +6,4 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 request(url + process.argv[2], function (err, response, body) {
   console.log(err || JSON.parse(body).title);
-});;
+});
