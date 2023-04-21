093c2a27b470f245bed63a4636f2ee76315677a8
Luis Gonzales <luisgonzacc@gmail.com>
2023-04-21 15:53:00 -0500

    fixing file

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-// This script prints the title of a Star Wars movie where
-// the episode number matches a given integer.
-const axios = require('axios');
-const ID = process.argv[2];
 
-axios.get(`https://swapi-api.hbtn.io/api/films/${ID}`)
-  .then(res => {
-    console.log(res.data.title);
-  });
+'use strict';
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + process.argv[2], function (err, response, body) {
+  console.log(err || JSON.parse(body).title);
+});;
