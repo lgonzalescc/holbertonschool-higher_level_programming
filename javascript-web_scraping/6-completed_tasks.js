bf758845df36025b33748c50ea4626c195db4e52
Luis Gonzales <luisgonzacc@gmail.com>
2023-04-21 16:09:16 -0500

    fix file

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,31 +1,26 @@
 #!/usr/bin/node
-const axios = require('axios').default;
 
-const api = process.argv[2];
-/**
- * Request status code.
- */
-axios.get(api)
-  .then(function (response) {
-    // handle success
-    const task = response.data;
-    const taskCompleted = {};
-    task.forEach(function (element) {
-      if (element.completed === true) {
-        const userId = element.userId;
-        if (taskCompleted[userId] === undefined) {
-          taskCompleted[userId] = 1;
+'use strict';
+
+const request = require('request');
+const url = process.argv[2];
+let a = '';
+let list = [];
+const dict = {};
+
+request(url, function (err, response, body) {
+  if (!err) {
+    list = JSON.parse(body);
+    for (let i = 0; i < list.length; i++) {
+      a = list[i].userId;
+      if (list[i].completed === true) {
+        if (dict[a]) {
+          dict[a] += 1;
         } else {
-          taskCompleted[userId] += 1;
+          dict[a] = 1;
         }
       }
-    });
-    console.log(taskCompleted);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error.message);
-  })
-  .then(function () {
-    // always executed
-  });
+    }
+    console.log(dict);
+  }
+});
