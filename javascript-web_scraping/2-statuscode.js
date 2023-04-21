586c0084e340cfdbe1cd0d3184e48d16a91b5b46
Luis Gonzales <luisgonzacc@gmail.com>
2023-04-21 15:43:05 -0500

    fixing file

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,19 +1,5 @@
 #!/usr/bin/node
-const axios = require('axios').default;
-
-/**
- * Request status code.
- */
-axios.get(process.argv[2])
-  .then(function (response) {
-    // handle success
-    console.log('code:', response.status);
-  })
-  .catch(function (error) {
-    // handle error
-    if (error.response) console.log('code:', error.response.status);
-    else console.log(error.message);
-  })
-  .then(function () {
-    // always executed
-  });
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});;
