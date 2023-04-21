34c176df25b7f19a27ece6ae5065c1a1d731e910
Luis Gonzales <luisgonzacc@gmail.com>
2023-04-21 16:06:45 -0500

    fix file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,13 +1,17 @@
 #!/usr/bin/node
-// This script gets the contents of a webpage and stores it in a file.
-const axios = require('axios');
-const fs = require('fs');
 
-axios.get(process.argv[2])
-  .then(res => {
-    fs.writeFile(process.argv[3], res.data, err => {
+'use strict';
+
+const request = require('request');
+const fs = require('fs');
+let txt;
+request(process.argv[2], function (err, response, body) {
+  if (!err) {
+    txt = body;
+    fs.writeFile(process.argv[3], txt, 'utf-8', (err) => {
       if (err) {
-        return console.log(err);
+        console.log(err);
       }
     });
-  });
+  }
+});
